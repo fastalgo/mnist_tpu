969418c2c486a7df5b1794116e98dcbca436fe9e
fastalgo <chinayouyang1991@gmail.com>
2019-03-05 03:57:07 +0000

    u

--- momentum_poly/run.sh
+++ momentum_poly/run.sh
@@ -1,4 +1,18 @@
-gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
 #export PYTHONPATH="${PYTHONPATH}:/usr/share/models/"
 #python /usr/share/models/official/mnist/mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000
-python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False
+gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
+python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False --learning_rate=0.08
+gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
+python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False --learning_rate=0.08
+gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
+python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False --learning_rate=0.08
+gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
+python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False --learning_rate=0.08
+gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
+python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False --learning_rate=0.09
+gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
+python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False --learning_rate=0.09
+gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
+python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False --learning_rate=0.09
+gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log/*
+python mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000 --eval_steps=10 --enable_predict=False --learning_rate=0.09
