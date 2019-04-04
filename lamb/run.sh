f5411fe74e13af7d9e033763379e4f678d10c53b
fastalgo <chinayouyang@gmail.com>
2019-04-04 01:36:31 +0000

    u

--- lamb/run.sh
+++ lamb/run.sh
@@ -2,7 +2,7 @@
 #python /usr/share/models/official/mnist/mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000
 for i in 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 9 8 7 6 5
 do
-	for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+	for j in 0.001 0.002 0.005 0.006 0.008 0.01 0.02 0.04 0.06 0.08 0.1
 	do
 		echo "warmup $i, LR $j"
 		gsutil -m rm -R -f gs://bert-pretrain-data/mnist_log3/*
